25a90f37be4fb7a784b51020c99c73d50c4524b6
Tony Fortes Ramos <amfortesramos@hotmail.com>
2020-06-24 23:08:02 +0000

    Ch2 - Ex1 done

---
+++ ch2/ex1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"ex1/tempconv"
+)
+
+func main() {
+	fmt.Println(tempconv.CToF(100))
+	fmt.Println(tempconv.CToK(0))
+	fmt.Println(tempconv.FToC(212))
+	fmt.Println(tempconv.FToK(0))
+	fmt.Println(tempconv.KToC(0))
+	fmt.Println(tempconv.KToC(100))
+}
