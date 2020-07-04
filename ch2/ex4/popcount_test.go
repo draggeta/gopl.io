7d11ab3f19257348171089a3dcc3b164c3d9b2fa
Tony Fortes Ramos <amfortesramos@hotmail.com>
2020-07-04 21:42:28 +0000

    Finish chapter 2

---
+++ ch2/ex4/popcount_test.go
@@ -0,0 +1,64 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 45.
+
+// (Package doc comment intentionally malformed to demonstrate golint.)
+
+// Ex: version of PopCount that counts by shifting 64 bits back.
+
+//!+
+package popcount
+
+import (
+	"testing"
+)
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountShift(x uint64) int {
+	var count int
+	test := uint64(1)
+
+	for i := 0; i < 64; i++ {
+		if x&test == 1 {
+			count++
+		}
+		x >>= 1
+	}
+
+	return count
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(uint64(i))
+	}
+}
+
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountShift(uint64(i))
+	}
+}
+
+//!-
