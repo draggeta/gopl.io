4f20c93f2105c27b99a02681add2272201b40a3f
Tony Fortes Ramos <amfortesramos@hotmail.com>
2020-07-25 22:52:26 +0000

    Finish chapter 3 ex 12

---
+++ ch3/ex12/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Please specify two strings.")
+		return
+	}
+
+	w1, w2 := os.Args[1], os.Args[2]
+	if len([]rune(w1)) != len([]rune(w2)) {
+		fmt.Println("Not anagrams")
+		return
+	}
+
+	w1Count := make(map[rune]int)
+	w2Count := make(map[rune]int)
+
+	for _, r := range w1 {
+		w1Count[r]++
+	}
+
+	for _, r := range w2 {
+		w2Count[r]++
+	}
+
+	if reflect.DeepEqual(w1Count, w2Count) {
+		fmt.Println("Are anagrams")
+	} else {
+		fmt.Println("Not anagrams")
+	}
+
+}
