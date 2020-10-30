ab66f2927eabb15bd0885d5c944178f553259247
Tony Fortes Ramos <amfortesramos@hotmail.com>
2020-10-30 15:42:06 +0000

    committed someone else's code used to compare, fixed

--- ch5/ex2/main.go
+++ ch5/ex2/main.go
@@ -1,41 +1,76 @@
-// ex5.2 counts the frequency of different tags in an html document on stdin.
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 122.
+//!+main
+
+// Findlinks1 prints the links in an HTML document read from standard input.
 package main
 
 import (
 	"fmt"
-	"io"
-	"log"
 	"os"
 
 	"golang.org/x/net/html"
 )
 
-func tagFreq(r io.Reader) (map[string]int, error) {
-	freq := make(map[string]int, 0)
-	z := html.NewTokenizer(os.Stdin)
-	var err error
-	for {
-		type_ := z.Next()
-		if type_ == html.ErrorToken {
-			break
-		}
-		name, _ := z.TagName()
-		if len(name) > 0 {
-			freq[string(name)]++
-		}
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
 	}
-	if err != io.EOF {
-		return freq, err
+	eCount := make(map[string]int)
+	countElem(eCount, doc)
+	for elem, count := range eCount {
+		fmt.Println(elem, count)
 	}
-	return freq, nil
 }
 
-func main() {
-	freq, err := tagFreq(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
+//!-main
+
+//!+visit
+// visit appends to links each link found in n and returns the result.
+func countElem(e map[string]int, n *html.Node) {
+	if n.Type == html.ElementNode {
+		e[n.Data]++
 	}
-	for tag, count := range freq {
-		fmt.Printf("%4d %s\n", count, tag)
+	if n.FirstChild != nil {
+		countElem(e, n.FirstChild)
+	}
+	if n.NextSibling != nil {
+		countElem(e, n.NextSibling)
 	}
 }
+
+//!-visit
+
+/*
+//!+html
+package html
+
+type Node struct {
+	Type                    NodeType
+	Data                    string
+	Attr                    []Attribute
+	FirstChild, NextSibling *Node
+}
+
+type NodeType int32
+
+const (
+	ErrorNode NodeType = iota
+	TextNode
+	DocumentNode
+	ElementNode
+	CommentNode
+	DoctypeNode
+)
+
+type Attribute struct {
+	Key, Val string
+}
+
+func Parse(r io.Reader) (*Node, error)
+//!-html
+*/
